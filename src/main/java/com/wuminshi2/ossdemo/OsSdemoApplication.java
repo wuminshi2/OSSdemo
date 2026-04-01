616a0022ffc7d3402dfb169a4ca0fc9a99d3238d
wuminshi2 <3309665946@qq.com>
2026-04-01 15:48:14 +0800

    初始化项目结构，导入aliyunOSS依赖

---
+++ src/main/java/com/wuminshi2/ossdemo/OsSdemoApplication.java
@@ -0,0 +1,13 @@
+package com.wuminshi2.ossdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsSdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsSdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wuminshi2/ossdemo/OsSdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuminshi2.ossdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsSdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
